03d35f6118d3c82b85f87381dea0d7ca8fc4957d
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-06-18 15:44:34 +0100

    Created dish model and demo data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,3 +1,4 @@
+import 'package:eatz/data/dish.dart';
 import 'package:eatz/models/category.dart';
 import 'package:eatz/utils/assets.dart';
 
@@ -8,4 +9,14 @@ List<Category> categories = [
   Category(icon: Assets.burger, label: 'Burger'),
   Category(icon: Assets.saladSvg, label: 'Salad'),
   Category(icon: Assets.rice, label: 'Rice'),
+];
+
+List<Dish> dishes = [
+  Dish(name: 'Fried Shrimps', image: Assets.friedShrimps, cook: 'Rex K', rating: 4.0, price: 20, description: 'description'),
+  Dish(name: 'Salad & Steak', image: Assets.saladSteak, cook: "BuBu's kitchen", rating: 5.0, price: 50, description: 'description'),
+  Dish(name: 'Burger', image: Assets.burger_1, cook: 'JideGuru', rating: 4.9, price: 25, description: 'description'),
+  Dish(name: 'Burger Pro', image: Assets.burger_2, cook: 'JideGuru', rating: 5.0, price: 30, description: 'description'),
+  Dish(name: 'Salad', image: Assets.salad, cook: "BuBu's kitchen", rating: 4.8, price: 25, description: 'description'),
+  Dish(name: 'Salad & Steak', image: Assets.saladSteak_2, cook: 'Rex K', rating: 5.0, price: 30, description: 'description'),
+  Dish(name: 'Beef Sandwiches', image: Assets.beefSandwiches, cook: 'Rex K', rating: 5.0, price: 35, description: 'description'),
 ];
---
+++ lib/data/dish.dart
@@ -0,0 +1,17 @@
+class Dish {
+  final String name;
+  final String image;
+  final String cook;
+  final double rating;
+  final double price;
+  final String description;
+
+  Dish({
+    required this.name,
+    required this.image,
+    required this.cook,
+    required this.rating,
+    required this.price,
+    required this.description,
+  });
+}
