802b7e124b08d7153d1df9f69b8ff23328853583
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-06-20 17:19:45 +0100

    Created head clipper background.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -19,10 +19,13 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
-            color: Colors.yellow.shade100,
+          ClipPath(
+            clipper: HeadClip(),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.7,
+              color: Colors.yellow.shade100,
+            ),
           ),
           SafeArea(
             bottom: false,
@@ -105,5 +108,23 @@ class _HomeState extends State<Home> {
         ],
       ),
     );
+  }
+}
+
+class HeadClip extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(size.width / 4, size.height, size.width, size.height / 1.5);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
